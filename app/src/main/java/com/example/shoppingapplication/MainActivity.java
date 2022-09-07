f9b5418a2fd5e74d15840891cdc8b694004286ea
Zofia Kaska <zkask001@gold.ac.uk>
2022-09-07 16:41:15 +0100

    Map Activity added, got maps to work on emulator and real device Map opens up through the User side (chosen on login page)

--- app/src/main/java/com/example/shoppingapplication/MainActivity.java
+++ app/src/main/java/com/example/shoppingapplication/MainActivity.java
@@ -67,4 +67,5 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
     }
+
 }
--- app/src/main/java/com/example/shoppingapplication/UserActivity.java
+++ app/src/main/java/com/example/shoppingapplication/UserActivity.java
@@ -2,7 +2,9 @@ package com.example.shoppingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+    }
+
+    public void launchMaps(View v){
+        //launch the map page
+
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
     }
 }
