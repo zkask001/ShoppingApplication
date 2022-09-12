7673d88ae99e6f58a827e1d6b07c8c44c433e79f
Zofia Kaska <zkask001@gold.ac.uk>
2022-09-12 17:44:41 +0100

    User is guided to activity dependant on their user type

--- app/src/main/java/com/example/shoppingapplication/LoginActivity2.java
+++ app/src/main/java/com/example/shoppingapplication/LoginActivity2.java
@@ -1,14 +1,127 @@
 package com.example.shoppingapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity2 extends AppCompatActivity {
+
+    //variables for each view initialised
+    EditText email, password;
+    Button loginBtn, goToRegister;
+    boolean valid = true;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        email = findViewById(R.id.LoginEmail);
+        password = findViewById(R.id.LoginPassword);
+        loginBtn = findViewById(R.id.LoginBtn);
+        goToRegister = findViewById(R.id.goToRegister);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkField(email);
+                checkField(password);
+
+                //if fields are valid, log in user
+                if(valid){
+                    fAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(LoginActivity2.this,"Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            //redirect user based on the user type (customer/supermarket):
+                            //check user type from user selection data at registration
+                            checkUserType(authResult.getUser().getUid());
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+
+        goToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+
+    }
+
+    private void checkUserType(String uid){
+        DocumentReference df = fStore.collection("Users").document(uid);
+        //extract data from document
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("TAG", "onSuccess" + documentSnapshot.getData());
+                //identify user type
+                if(documentSnapshot.getString("isUser") != null){
+                    //user is customer
+
+                    startActivity(new Intent(getApplicationContext(),UserActivity.class));
+                    finish();
+                }
+
+                if(documentSnapshot.getString("isSupermarket") != null){
+                    //user is supermarket
+
+                    startActivity(new Intent(getApplicationContext(),SupermarketActivity.class));
+                    finish();
+                }
+            }
+        })
+    }
+
+    //method to check if all fields are filled in by user
+    public boolean checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            //display error message if all fields are not filled in
+            textField.setError("Error");
+            valid = false;
+        }else{
+            valid = true;
+        }
+
+        return valid;
+    }
+
+    //check that no other user is logged in
+    public void onStart() {
+
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
     }
 }
