482beba949c8a0fa18ddc25ac347710d2ebee39c
Zofia Kaska <zkask001@gold.ac.uk>
2022-09-15 13:35:33 +0100

    Touch coordinates of floorplan image display as toast message (bitmap and canvas not yet working for image)

--- app/src/main/java/com/example/shoppingapplication/SupermarketActivity.java
+++ app/src/main/java/com/example/shoppingapplication/SupermarketActivity.java
@@ -3,8 +3,17 @@ package com.example.shoppingapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Picture;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -14,6 +23,33 @@ public class SupermarketActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supermarket);
+
+        addTouchListener();
+
+//        ImageView imageView=(ImageView) findViewById(R.id.floorplan);
+//        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+//        Canvas canvas = new Canvas(bitmap);
+//        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//        paint.setColor(Color.BLACK);
+//        canvas.drawColor(Color.TRANSPARENT);
+//        canvas.drawCircle(50, 50, 10, paint);
+//        imageView.setImageBitmap(bitmap);
+    }
+
+    private void addTouchListener(){
+        ImageView floorplan = (ImageView) findViewById(R.id.floorplan);
+        floorplan.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                float x = motionEvent.getX();
+                float y = motionEvent.getY();
+
+                Toast.makeText(SupermarketActivity.this, "Coordinates: x= " + x + " y= " + y,Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
     }
 
     public void logout(View view){
